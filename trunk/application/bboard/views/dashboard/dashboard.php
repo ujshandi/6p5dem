c3ba341161c43393339d50166eaca8d0b2cf2513
fajar.scripts@gmail.com <fajar.scripts@gmail.com@cafc2922-98ec-0af7-2b36-2e7d7a88d0bf>
2013-05-25 08:42:22 +0000

    edited layouts/footer.php edited layouts/header.php edited layouts/menu.php edited dashboard/dashboard.php

--- trunk/application/bboard/views/dashboard/dashboard.php
+++ trunk/application/bboard/views/dashboard/dashboard.php
@@ -1,110 +1,128 @@
-	<!-- Content -->
-	<article class="container_12">
-		
-		<section class="grid_4">
-			<!--<div class="block-border"><div class="block-content">-->
-				<h1>Aplikasi</h1>
-				
-				<ul class="favorites no-margin with-tip" title="Context menu available!">
-					
-					<li>
-						<img src="<?=base_url()?>asset/admin/images/icons/web-app/48/add.png" width="48" height="48">
-						<a href="../app_sdm">DATA SDM<br>						
-						<small>Pengelolaan Data SDM  </small></a>
-					</li>
-					
-					<li>
-						<img src="<?=base_url()?>asset/admin/images/icons/web-app/48/loading.png" width="48" height="48">
-						<a href="../app_diklat">DATA DIKLAT<br>
-						<small>Pengelolaan Data Diklat</small></a>
-					</li>
-					
-					<li>
-						<img src="<?=base_url()?>asset/admin/images/icons/web-app/48/modify.png" width="48" height="48">
-						<a href="../app_kompetensi">DATA KOMPETENSI<br>
-						<small>Pengelolaan Data Kompetensi</small></a>
-					</li>
-					
-					<li>
-						<img src="<?=base_url()?>asset/admin/images/icons/web-app/48/line-chart.png" width="48" height="48">
-						<a href="../app_gis">GIS<br>
-						<small>Sistem Informasi Geografis</small></a>
-					</li>
-					
-					<li>
-						<img src="<?=base_url()?>asset/admin/images/icons/web-app/48/bar-chart.png" width="48" height="48">
-						<a href="../app_dashboard">DASHBOARD<br>
-						<small>Monitoring Dashboard</small></a>
-					</li>
-					
-					<li>
-						<img src="<?=base_url()?>asset/admin/images/icons/web-app/48/Check.png" width="48" height="48">
-						<a href="../app_admin" >ADMINISTRASI</a>
-						<!--
-						<a title="Layanan Pihak Ke 3" href="<?=base_url().'index.php/layanan_jasa_kredit'?>"><img src="<?=base_url()?>asset/admin/images/icons/web-app/48/Adira-Finance2.png" width="92" height="32"></a>
-						<a title="Layanan Speedy" href="<?=base_url().'index.php/layanan_jasa_speedy'?>"><img src="<?=base_url()?>asset/admin/images/icons/web-app/48/speedy.png" width="92" height="32"></a>
-						<a title="Layanan Listrik" href="<?=base_url().'index.php/layanan_jasa_listrik'?>"><img src="<?=base_url()?>asset/admin/images/icons/web-app/48/pln.png" width="92" height="32"></a>
-						<a title="Layanan PDAM" href="<?=base_url().'index.php/layanan_jasa_pdam'?>"><img src="<?=base_url()?>asset/admin/images/icons/web-app/48/pdam.png" width="92" height="32"></a>
-						<a title="Layanan Telepon" href="<?=base_url().'index.php/layanan_jasa_telepon'?>"><img src="<?=base_url()?>asset/admin/images/icons/web-app/48/telkom.jpg" width="92" height="32"></a>
-					    -->
-					</li>
-					
-				</ul>
-				
-				
-			<!--</div></div>-->
-		</section>
-		
-		<section class="grid_80">
-			<div class="block-border"><div class="block-content">
-				<div class="h1 with-menu">
-					<h1>BERITA & INFORMASI</h1>
-					
-				</div>
-			
-				<div class="block-controls">
-					
-					<ul class="controls-tabs js-tabs same-height with-children-tip">
-						<li><a href="#tab-stats" title="&nbsp" width="24" height="24"></a></li>
-					</ul>
-					
-				</div>
-				
-				<form class="form" id="tab-stats" method="post" action="">						
-						
-					<div class="task with-legend">
-							<div class="legend"><img src="images/icons/fugue/status-away.png" width="16" height="16">BERITA</div>
-							
-							<div class="task-description">		
-								<div align="justify">
-								<br> </br>
-								<center><img src="<?=base_url()?>asset/admin/images/icons/web-app/48/saweri.jpg"></center>
-								<br>
-								 
-								<center><img src="<?=base_url()?>asset/admin/images/icons/web-app/48/saweri2.jpg"></center>
-								 
-								<br>
-								<h3>Diklat Pengembangan SDM</h3>
-								
-								<br>Diklat Pengembangan SDM Angkatan ke III akan dilaksanakan 1 s/d 14 Juni 2013. </br>
-								 
-								</div>
-							</div>
-							</ul>
-					</div>
-				</form>
-				
-				<ul class="message no-margin">
-					<li> <strong>@copyright</strong></li>
-				</ul>
-				
-			</div></div>
-		</section>
-		
-		<div class="clear"></div>
-		
-	</article>
-	
-	<!-- End content -->
-	
-	
+<div id="wrap">	
+	<div class="splash"><img src="<?=base_url()?>asset/board/asset/frontpage/images/splash.png" /></div>   
+	<div class="clear"></div>
+   
+  <div class="center_content">
+  
+    <div class="page_title">
+		<div id="h1_pagetitle"><h1>BPSDM Aplikasi</h1></div>
+		<div id="logout"><h3><?php echo logged_as();?>  <a href="<?php echo site_url();?>/auth/logout">Logout</a></h3></div>
+		<hr/>
+	</div>
+    
+    <div class="section_13">
+      <img src="<?=base_url()?>asset/board/asset/frontpage/images/icon1.png" alt="" title="" border="0" class="feat_thumb" />
+      <div class="feat_details">
+      <h4>Basis Data SDM</h4>
+      <a href="<?php echo base_url();?>sdm.php" class="inlink clear">ENTER</a>
+      <p>Duis aute irure dolor in reprehenderit in voluptate esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+    </div>
+    
+    <div class="section_13">
+      <img src="<?=base_url()?>asset/board/asset/frontpage/images/icon2.png" alt="" title="" border="0" class="feat_thumb" />
+      <div class="feat_details">
+      <h4>Basis Data Diklat</h4>
+      <a href="<?php echo base_url();?>diklat.php" class="inlink clear">ENTER</a>
+      <p>Duis aute irure dolor in reprehenderit in voluptate esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+    </div>
+    
+     <div class="section_13">
+      <img src="<?=base_url()?>asset/board/asset/frontpage/images/icon3.png" alt="" title="" border="0" class="feat_thumb" />
+      <div class="feat_details">
+      <h4>Manajemen Kompetensi</h4>
+      <a href="<?php echo base_url();?>mpp.php" class="inlink clear">ENTER</a>
+      <p>Duis aute irure dolor in reprehenderit in voluptate esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+    </div>
+    
+    <div class="section_13">
+      <img src="<?=base_url()?>asset/board/asset/frontpage/images/icon4.png" alt="" title="" border="0" class="feat_thumb" />
+      <div class="feat_details">
+      <h4>GIS SDM Transportasi</h4>
+      <a href="<?php echo base_url();?>gis.php" class="inlink clear">ENTER</a>
+      <p>Duis aute irure dolor in reprehenderit in voluptate esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+    </div>
+    
+    <div class="section_13">
+      <img src="<?=base_url()?>asset/board/asset/frontpage/images/icon5.png" alt="" title="" border="0" class="feat_thumb" />
+      <div class="feat_details">
+      <h4>Dashboard Monitoring</h4>
+      <a href="<?php echo base_url();?>dashbaord.php" class="inlink clear">ENTER</a>
+      <p>Duis aute irure dolor in reprehenderit in voluptate esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+    </div>
+    
+     <div class="section_13">
+      <img src="<?=base_url()?>asset/board/asset/frontpage/images/icon6.png" alt="" title="" border="0" class="feat_thumb" />
+      <div class="feat_details">
+      <h4>Basis Data Peraturan Transportasi</h4>
+      <a href="#" class="inlink clear">ENTER</a>
+      <p>Duis aute irure dolor in reprehenderit in voluptate esse cillum dolore eu fugiat nulla pariatur.</p>
+      </div>
+    </div>
+    
+    <hr/>
+	<div class="clear"></div>     
+  </div><!--end center content 1-->
+  
+  <div class="center_content">
+    
+    <div class="section_23">
+    <h2>File Sharing</h2>
+    <div class="file_box">
+    <h3>File Explorer</h3>
+    <hr/>
+    	<ul class="jtree">
+        	<li><span data-state="open" data-style="unclosable">root</span>
+            	<ul>
+                	<li><span>pictures</span>
+                    	<ul>
+                        	<li><span>pictures subfolder 1</span></li>
+                            <li><span>pictures subfolder 2</span></li>
+                            <li><span>pictures subfolder 3</span></li>
+                            <li><span>pictures subfolder 4</span></li>
+                            <li><span>pictures subfolder 5</span></li>
+                            <li><span>pictures subfolder 6</span></li>
+                        </ul>
+                    </li>
+                    <li><span>admin</span>
+                    	<ul>
+                        	<li><span>admin subfolder</span></li>
+                        </ul>
+                    </li>
+                    <li><span>img</span>
+                    	<ul>
+                        	<li><span>img subfolder</span></li>
+                        </ul>
+                    </li>
+                    <li><span>js</span>
+                    	<ul>
+                        	<li><span>js subfolder</span></li>
+                        </ul>
+                    </li>
+                    <li><span>plugin</span>
+                    	<ul>
+                        	<li><span>plugin subfolder</span></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div><!--end file box-->
+    </div>
+    
+    <div class="section_14">
+    	<h2>Calendar</h2>
+        <hr/>
+        <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit</p>
+        <hr/>
+        <div id="calendar_box"></div>
+    </div>
+    
+	<div class="clear"></div> 
+  </div>
+  
+</div><!--end center content 2-->
--- trunk/asset/board/asset_saweri/admin/js/ajax_penerimaan_kas.php
+++
@@ -1,92 +0,0 @@
-<?php
-	#
-	include('convert_rupiah.php');
-	
-	# memasukan detail barang ke list detail di form add barang
-	function add_detail_1() // pas add
-	{
-		$data['detail_akunid'] 		= $_POST['detail_akunid'];
-		$data['detail_nakun'] 		= $_POST['detail_nakun'];
-		$data['detail_jumlah']	 	= $_POST['detail_jumlah'];		$data['detail_count']	 	= $_POST['detail_count'];
-		
-		$i=0;
-		//print_r($detail);
-		
-		if (isset($_POST['detail']))
-		{
-			$detail = $_POST['detail'];
-			$count_detail = count($detail);
-		
-			for($i=0; $i<$count_detail; $i++)
-			{
-				echo '						<tr>							<td>'.($i + 1).'</td>														<td>								'.$detail[$i]['akunid'].'								<input type="hidden" name="detail['.$i.'][akunid]" value="'.$detail[$i]['akunid'].'" />							</td>														<td>								'.$detail[$i]['nakun'].'								<input type="hidden" name="detail['.$i.'][nakun]" value="'.$detail[$i]['nakun'].'" />							</td>							<td align="right">								'.convert_rupiah($detail[$i]['jumlah']).'								<input type="hidden" name="detail['.$i.'][jumlah]" value="'.$detail[$i]['jumlah'].'" />							</td>														<td>								<input type="text" name="detail['.$i.'][ket_transaksi]" value="" />							</td>						</tr>';
-			}
-		}
-		
-		$xx=0;
-		for($xx=0; $xx < $data['detail_count']; $xx++){
-			echo '
-						<tr>							<td>'.($i + 1).'</td>							<td>								'.$data['detail_akunid'].'								<input type="hidden" name="detail['.$i.'][akunid]" value="'.$data['detail_akunid'].'" />							</td>														<td>								'.$data['detail_nakun'].'								<input type="hidden" name="detail['.$i.'][nakun]" value="'.$data['detail_nakun'].'" />							</td>							<td align="right">								'.convert_rupiah($data['detail_jumlah']).'								<input type="hidden" name="detail['.$i.'][jumlah]" value="'.$data['detail_jumlah'].'" />							</td>							<td>								<input type="text" name="detail['.$i.'][ket_transaksi]" value="" />							</td>						</tr>';
-			$i++;
-		}
-	}
-
-	function add_detail_2() // pas edit
-	{
-		$data['detail_akunid'] 		= $_POST['detail_akunid'];		$data['detail_nakun'] 		= $_POST['detail_nakun'];		$data['detail_jumlah']	 	= $_POST['detail_jumlah'];		$data['detail_count']	 	= $_POST['detail_count'];
-		
-		$i=0;
-		
-		
-		if (isset($_POST['detail']))
-		{
-			$detail = $_POST['detail'];
-			$count_detail = count($detail);
-		
-			for($i=0; $i<$count_detail; $i++)
-			{
-				echo '
-						<tr>							<td>'.($i + 1).'</td>							<td>								'.$data['detail_akunid'].'								<input type="hidden" name="detail['.$i.'][akunid]" value="'.$data['detail_akunid'].'" />							</td>														<td>								'.$data['detail_nakun'].'								<input type="hidden" name="detail['.$i.'][nakun]" value="'.$data['detail_nakun'].'" />							</td>															<td>								'.convert_rupiah($data['detail_jumlah']).'								<input type="hidden" name="detail['.$i.'][jumlah]" value="'.$data['detail_jumlah'].'" />							</td>														<td>								<input type="text" name="detail['.$i.'][ket_transaksi]" value="" />							</td>						</tr>';
-			}
-		}
-		
-		
-		$xx=0;
-		for($xx=0; $xx < $data['detail_count']; $xx++){
-			echo '
-						<tr>							<td>'.($i + 1).'</td>														<td>								'.$data['detail_akunid'].'								<input type="hidden" name="detail['.$i.'][akunid]" value="'.$data['detail_akunid'].'" />							</td>																					<td>								'.$data['detail_nakun'].'								<input type="hidden" name="detail['.$i.'][nakun]" value="'.$data['detail_nakun'].'" />							</td>																						<td>								'.convert_rupiah($data['detail_jumlah']).'								<input type="hidden" name="detail['.$i.'][jumlah]" value="'.$data['detail_jumlah'].'" />							</td>							<td>								<input type="text" name="detail['.$i.'][ket_transaksi]" value="" />							</td>						</tr>';
-			$i++;
-		}
-	}
-	
-	function remove_detail($id)
-	{
-		if (isset($_POST['detail']))
-		{
-			$detail = $_POST['detail'];
-			$count_detail = count($detail);
-			
-			for($x=0; $x<$count_detail; $x++)			{				if($id != $x){					echo '							<tr>								<td>'.($i + 1).'</td>																<td>									'.$detail[$i]['detail_akunid'].'									<input type="hidden" name="detail['.$i.'][akunid]" value="'.$detail[$i]['detail_akunid'].'" />								</td>																<td>									'.$detail[$i]['detail_nakun'].'									<input type="hidden" name="detail['.$i.'][nakun]" value="'.$detail[$i]['detail_nakun'].'" />								</td>								<td>									'.convert_rupiah($detail[$i]['jumlah']).'									<input type="hidden" name="detail['.$i.'][jumlah]" value="'.$detail[$i]['detail_jumlah'].'" />								</td>																<td>								<input type="text" name="detail['.$i.'][ket_transaksi]" value="" />							</td>								<td align="center">									<a href="Javascript:remove_detail('.$i.')">Batal</a>								</td>																							</tr>';					$i++;				}			}
-		}
-	}
-	
-	$command = $_GET['command'];
-		
-	if($command == 'add_1')
-	{
-		add_detail_1();
-	
-	}else if($command == 'add_2')
-	{
-		add_detail_2();
-	
-	}else if($command == 'remove')
-	{
-		$id = $_GET['id'];
-		remove_detail($id);
-		
-	}else if($command == 'change_total')
-	{
-		echo convert_rupiah($_GET['total']);
-	}
-?>
