4938b1320e272a07f381e20cad0fed688350d6f4
stefanus.christian.c <stefanus.christian.c@cafc2922-98ec-0af7-2b36-2e7d7a88d0bf>
2013-05-25 14:36:07 +0000

    Penambahan Menu Dashboard Monitoring - Sementara Baru layout - Merubah folder asset, dan menambahkan dbmon.php dan folder application/dbmon

---
+++ trunk/application/dbmon/views/layout/menu.php
@@ -0,0 +1,40 @@
+
+<div id="top"> 
+    
+<div class="menu-title">Dashboard Monitoring</div><!-- end page title -->
+<div class="submenu">
+    <ul>
+        <li class="current"><a href="#">Dashboard Monitoring SDM</a>
+		</li>
+        <li><a href="#">Dashboard Monitoring Diklat</a></li>
+        <li><a href="#">Dashboard Monitoring Kompetensi</a></li>
+    </ul>
+</div><!-- end submenu -->
+<ul class="homout">
+  <li><a href="#"><img src="<?=base_url()?>asset/diklat/images/icon_home_16x16.png" />HOME</a></li>
+  <li><a href="#"><img src="<?=base_url()?>asset/diklat/images/icon_logout_16x16.png" />LOGOUT</a></li>
+</ul>
+
+</div><!-- end topmenu -->
+
+  <div id="content">
+    <div id="contentwrap">
+    	<div class="wrap_left bgtrans">
+			<h2 class="heading">Side Menu</h2>
+			<hr/>
+			<ul id="vmenu">
+				<li>
+					<a href="#">Dashboard Monitoring SDM</a>
+					<ul>
+						<li><a href="#">Komposisi SDM Dinas</a></li>
+						<li><a href="#">Komposisi SDM Kementrian</a></li>
+						<li><a href="#">Komposisi SDM Non-Aparatur</a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="#">Dashboard Monitoring Diklat</a>
+				</li>
+				<li><a href="#">Dashboard Monitoring Kompetensi</a></li>
+			</ul><!-- end vmenu -->
+		</div><!-- end wrap left-->
+    
