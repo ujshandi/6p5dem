f96419919264437cf2951168567a70d4051896a2
indriyanto.net@gmail.com <indriyanto.net@gmail.com@cafc2922-98ec-0af7-2b36-2e7d7a88d0bf>
2013-05-26 03:09:33 +0000

    - modif session - add menu sdm

--- trunk/application/sdm/config/autoload.php
+++ trunk/application/sdm/config/autoload.php
@@ -109,7 +109,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array('authentikasi');
+$autoload['model'] = array('Authentikasi');
 
 
 /* End of file autoload.php */
--- trunk/application/sdm/config/database.php
+++ trunk/application/sdm/config/database.php
@@ -49,10 +49,10 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'bpsdm_new';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['username'] = 'BPSDM';
+$db['default']['password'] = 'BPSDM';
+$db['default']['database'] = 'BPSDM';
+$db['default']['dbdriver'] = 'oci8';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
