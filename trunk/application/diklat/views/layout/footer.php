e5d9e9e69868ae94a33cab62e3729289e97ae4d0
iqbal4692@gmail.com <iqbal4692@gmail.com@cafc2922-98ec-0af7-2b36-2e7d7a88d0bf>
2013-05-29 11:04:11 +0000

    desain diklat v1.2

--- trunk/application/diklat/views/layout/footer.php
+++ trunk/application/diklat/views/layout/footer.php
@@ -1,5 +1,4 @@
-
-    <div class="clearfix"></div>
+<div class="clearfix"></div>
     </div><!-- end content wrap -->
   </div><!-- end content -->
   
--- trunk/application/diklat/views/layout/header.php
+++ trunk/application/diklat/views/layout/header.php
@@ -5,8 +5,8 @@
 	<title>Sistem Informasi Manajemen SDM Perhubungan - BPSDM Perhubungan - Kementerian Perhubungan RI</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	
-	<link rel="shortcut icon" href="<?=base_url()?>asset/diklat/images/favicon.ico" />
-	<link type="text/css" href="<?=base_url()?>asset/diklat/css/style.css" rel="stylesheet"  />
+	<link rel="shortcut icon" href="<?=base_url()?>asset/diklat/favicon.ico" />
+	<link type="text/css" href="<?=base_url()?>asset/diklat/css/style.css" rel="stylesheet"  type="text/css"  />
 	<script type="text/javascript" src="<?=base_url()?>asset/diklat/js/jquery-min.js" ></script>
 	<script type="text/javascript" src="<?=base_url()?>asset/diklat/js/custom.js" ></script>
 </head>
@@ -14,8 +14,8 @@
 <body>
 <div id="container">
 
-<div id="header"> 
-    <div class="header-img"> 
+<div id="header" class="bg_green"> 
+    <div class="header-img green"> 
       <div id="title">
         <h3>BPSDM Perhubungan - Kementerian Perhubungan RI</h3>
         <h2>Sistem Informasi Manajemen SDM Bidang Transportasi</h2>
@@ -25,7 +25,24 @@
       <div class="hublogo"><a href="http://www.dephub.go.id" title="Kementerian Perhubungan RI" target="_blank"></a></div>
       <div class="clear"></div>
       
-      <div class="usr"><span>Selamat Datang: </span><a href="#"><?=$NAMA_LENGKAP?></a></div>
+      <div class="bread">
+      	<ul class="breadcrumbs">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Aplikasi title</a></li>
+          <li><span>Edit</span></li>
+          <li class="current"><a href="#">Add</a></li>
+        </ul>
+      </div><!-- end breadcrumbs -->
+      
+      <div class="right">
+          <div class="usr"><span>welcome user</span><a href="#">Lukito Wibowo</a></div>
+            <ul class="homout">
+                <li><a href="#"><img src="<?=base_url()?>asset/diklat/images/icon_home_16x16.png" />HOME</a></li>
+                <li><a href="#"><img src="<?=base_url()?>asset/diklat/images/icon_logout_16x16.png" />LOGOUT</a></li>
+            </ul>
+      </div>
+      
+      <div class="clear"></div>
     </div><!-- end image headers -->
     
 </div><!-- end header -->
