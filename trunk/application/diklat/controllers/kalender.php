07ec8eaf34f2989520c617ce6a7a906983d26db9
iqbal4692@gmail.com <iqbal4692@gmail.com@cafc2922-98ec-0af7-2b36-2e7d7a88d0bf>
2013-06-03 12:38:37 +0000

    - Menampilkan form List
    -Tambah data dosen (idnya masih di input. karena mash cari tau bikin auto increment di ci - oracle)

---
+++ trunk/application/diklat/controllers/kalender.php
@@ -0,0 +1,124 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class kalender extends My_Controller {
+	
+	function __construct(){
+		parent::__construct();
+		$this->load->model('mdl_upt');
+		$this->load->model('mdl_kalender');
+	}
+	
+	public function index()
+	{
+		$this->open();
+		
+		# config pagination
+		$config['base_url'] = base_url().'/'.$this->config->item('index_page').'/kalender/index/';
+		$config['total_rows'] = $this->db->count_all('DIKLAT_KALENDER');
+		$config['per_page'] = '30';
+		$config['num_links'] = '3';
+		// $config['uri_segment'] = '3';
+		// $config['full_tag_open'] = '';
+		// $config['full_tag_close'] = '';
+		// $config['num_tag_open'] = '<li>';
+		// $config['num_tag_close'] = '</li>';
+		// $config['cur_tag_open'] = '<li><a href="javascript:void(0)" class="current">';
+		// $config['cur_tag_close'] = '</a></li>';
+		// $config['prev_link'] = 'Prev';
+		// $config['prev_tag_open'] = '<li>';
+		// $config['prev_tag_close'] = '</li>';
+		// $config['next_link'] = 'Next';
+		// $config['next_tag_open'] = '<li>';
+		// $config['next_tag_close'] = '</li>';
+		// $config['last_link'] = 'Last';
+		// $config['last_tag_open'] = '<li>';
+		// $config['last_tag_close'] = '</li>';
+		// $config['first_link'] = 'First';
+		// $config['first_tag_open'] = '<li>';
+		// $config['first_tag_close'] = '</li>';
+
+		$this->pagination->initialize($config);	
+		
+		$data['result'] = $this->mdl_kalender->getData($config['per_page'], $this->uri->segment(3));
+		$this->load->view('kalender/kalender_list', $data);
+		
+		$this->close();
+	}
+	
+	// public function add(){
+		// $this->open();
+		// $this->load->view('kalender/kalender_add');
+		// $this->close();
+	// }
+	
+	// public function proses_add(){
+		// $this->open();
+		
+		// # get post data
+		// $data['KODE_kalender'] = $this->input->post('KODE_kalender');
+        // $data['NAMA_kalender'] = $this->input->post('NAMA_kalender');
+        // $data['KODE_INDUK'] = $this->input->post('KODE_INDUK');
+		
+		// # set rules validation
+		// $this->form_validation->set_rules('KODE_kalender', 'KODE kalender', 'required');
+        // $this->form_validation->set_rules('NAMA_kalender', 'NAMA kalender', 'required');
+        // $this->form_validation->set_rules('KODE_INDUK', 'KODE INDUK', 'required');
+        //$this->form_validation->set_rules('URUTAN', 'URUTAN', 'required');
+		
+		// # set message validation
+		// $this->form_validation->set_message('required', 'Field %s harus diisi!');
+		
+		// if ($this->form_validation->run() == FALSE){
+			// $this->load->view('kalender/kalender_add',$data);
+		// }else{
+			// $this->mdl_kalender->insert($data);
+			// redirect('kalender');
+		// }
+		
+		// $this->close();
+	// }
+	
+	// public function edit($id){
+		// $this->open();
+		
+		// $data['id'] = $id;
+		// $data['result'] = $this->mdl_kalender->getDataEdit($id);
+		// $this->load->view('kalender/kalender_edit', $data);
+		
+		// $this->close();
+	// }
+	
+	// public function proses_edit(){
+		// $this->open();
+		
+		// $data['id'] = $this->input->post('id');
+		// $data['KODE_kalender'] = $this->input->post('KODE_kalender');
+        // $data['NAMA_kalender'] = $this->input->post('NAMA_kalender');
+        // $data['KODE_INDUK'] = $this->input->post('KODE_INDUK');
+		
+		// # set rules validation
+		// $this->form_validation->set_rules('KODE_kalender', 'KODE kalender', 'required');
+        // $this->form_validation->set_rules('NAMA_kalender', 'NAMA kalender', 'required');
+        // $this->form_validation->set_rules('KODE_INDUK', 'KODE INDUK', 'required');
+		// # set message validation
+		// $this->form_validation->set_message('required', 'Field %s harus diisi!');
+		
+		// if ($this->form_validation->run() == FALSE){
+			// $this->load->view('kalender/kalender_edit',$data);
+		// }else{
+			// $this->mdl_kalender->update($data);
+			// redirect('kalender');
+		// }
+		
+		// $this->close();
+	// }
+	
+	// public function proses_delete($id){
+		// if($this->mdl_kalender->delete($id)){
+			// redirect('kalender');
+		// }else{
+			//code u/ gagal simpan
+		// }
+	// }
+	
+}
---
+++ trunk/application/diklat/views/home.php
@@ -0,0 +1,14 @@
+<!-- contenna -->
+<div class="wrap_right bgcontent">
+	<h1 class="heading">Aplikasi  Basis Data Diklat</h1>
+	<hr/>
+        <p class="text">
+		   Aplikasi Basis Data Diklat Berfungsi untuk mengelola Data Diklat yang dikelola oleh BPSDM
+		</p>
+		<p class="text">
+		   Silahkan mengakses menu yang tersedia sesuai dengan kebutuhan anda
+		</p>
+     <hr/>
+	<div class="clear">&nbsp;</div>
+	 
+</div><!-- end wrap right content-->
