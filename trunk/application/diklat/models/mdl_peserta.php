47d1748ac0c1db10b6315576c08bf137fd9938d4
iqbal4692@gmail.com <iqbal4692@gmail.com@cafc2922-98ec-0af7-2b36-2e7d7a88d0bf>
2013-06-14 13:18:47 +0000

    coba chaine combobox
    http://dokumenary.wordpress.com/2012/01/20/ajax-2-combobox-saling-berhubungan-pada-codeigniter/

--- trunk/application/diklat/models/mdl_peserta.php
+++ trunk/application/diklat/models/mdl_peserta.php
@@ -25,9 +25,9 @@ class mdl_peserta extends CI_Model{
         $this->db->set('NO_PESERTA', $data['NO_PESERTA']);
         $this->db->set('NAMA_PESERTA', $data['NAMA_PESERTA']);
         $this->db->set('TEMPAT_LAHIR', $data['TEMPAT_LAHIR']);
-        $this->db->set('TGL_LAHIR', $data['TGL_LAHIR']);
+        $this->db->set('TGL_LAHIR', $data['TGL_LAHIR'], false);
         $this->db->set('JK', $data['JK']);
-        $this->db->set('TGL_MASUK', $data['TGL_MASUK']);
+        $this->db->set('TGL_MASUK', $data['TGL_MASUK'], false);
         $this->db->set('THN_ANGKATAN', $data['THN_ANGKATAN']);
         $this->db->set('STATUS_PESERTA', $data['STATUS_PESERTA']);
         $this->db->set('KETERANGAN', $data['KETERANGAN']);
@@ -58,13 +58,13 @@ class mdl_peserta extends CI_Model{
         $this->db->set('NO_PESERTA', $data['NO_PESERTA']);
         $this->db->set('NAMA_PESERTA', $data['NAMA_PESERTA']);
         $this->db->set('TEMPAT_LAHIR', $data['TEMPAT_LAHIR']);
-        $this->db->set('TGL_LAHIR', $data['TGL_LAHIR']);
+        $this->db->set('TGL_LAHIR', $data['TGL_LAHIR'], false);
         $this->db->set('JK', $data['JK']);
-        $this->db->set('TGL_MASUK', $data['TGL_MASUK']);
+        $this->db->set('TGL_MASUK', $data['TGL_MASUK'], false);
         $this->db->set('THN_ANGKATAN', $data['THN_ANGKATAN']);
         $this->db->set('STATUS_PESERTA', $data['STATUS_PESERTA']);
         $this->db->set('KETERANGAN', $data['KETERANGAN']);
-		$this->db->where('KODE_PROGRAM', $data['id']);
+		$this->db->where('IDPESERTA', $data['id']);
 		
 		$result = $this->db->update('DIKLAT_MST_PESERTA');
 		
@@ -89,5 +89,114 @@ class mdl_peserta extends CI_Model{
 		
 	}
 	
+	function getOptionJenkel($d=""){
+		$name = isset($d['name'])?$d['name']:'';
+		$id = isset($d['id'])?$d['id']:'';
+		$class = isset($d['class'])?$d['class']:'';
+		$value = isset($d['value'])?$d['value']:'';
+		
+		$res = array('Pria','Wanita');
+		
+		$out = '<select name="'.$name.'" id="'.$id.'">';
+		foreach($res as $r){
+			if($r == trim($value)){
+				$out .= '<option value="'.$r.'" selected="selected">'.$r.'</option>';
+			}else{
+				$out .= '<option value="'.$r.'">'.$r.'</option>';
+			}
+		}
+		$out .= '</select>';
+		
+		return $out;
+	}
+	
+	function getOptionStatus($d=""){
+		$name = isset($d['name'])?$d['name']:'';
+		$id = isset($d['id'])?$d['id']:'';
+		$class = isset($d['class'])?$d['class']:'';
+		$value = isset($d['value'])?$d['value']:'';
+		
+		$res = array('Registrasi','Drop Out', 'Lulus');
+		
+		$out = '<select name="'.$name.'" id="'.$id.'">';
+		foreach($res as $r){
+			if($r == trim($value)){
+				$out .= '<option value="'.$r.'" selected="selected">'.$r.'</option>';
+			}else{
+				$out .= '<option value="'.$r.'">'.$r.'</option>';
+			}
+		}
+		$out .= '</select>';
+		
+		return $out;
+	}
+	
+	function getOptionTahun($d=""){
+		$name = isset($d['name'])?$d['name']:'';
+		$id = isset($d['id'])?$d['id']:'';
+		$class = isset($d['class'])?$d['class']:'';
+		$value = isset($d['value'])?$d['value']:'';
+		
+		$res = array(2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020);
+		
+		$out = '<select name="'.$name.'" id="'.$id.'">';
+		foreach($res as $r){
+			if($r == trim($value)){
+				$out .= '<option value="'.$r.'" selected="selected">'.$r.'</option>';
+			}else{
+				$out .= '<option value="'.$r.'">'.$r.'</option>';
+			}
+		}
+		$out .= '</select>';
+		
+		return $out;
+	}
+	
+	function getOptionPeserta($d=""){
+		$name = isset($d['name'])?$d['name']:'';
+		$id = isset($d['id'])?$d['id']:'';
+		$class = isset($d['class'])?$d['class']:'';
+		$value = isset($d['value'])?$d['value']:'';
+		
+		$this->db->flush_cache();
+		$this->db->from('DIKLAT_MST_PESERTA');
+		$this->db->order_by('NO_PESERTA');
+		
+		$res = $this->db->get();
+		
+		$out = '<select name="'.$name.'" id="'.$id.'">';
+		foreach($res->result() as $r){
+			if($r->NO_PESERTA == trim($value)){
+				$out .= '<option value="'.$r->NO_PESERTA.'" selected="selected">'.$r->NAMA_PESERTA.'</option>';
+			}else{
+				$out .= '<option value="'.$r->NO_PESERTA.'">'.$r->NAMA_PESERTA.'</option>';
+			}
+		}
+		$out .= '</select>';
+		
+		return $out;
+	}
+	
+	function getUPT(){
+		$result = this->db->get('DIKLAT_MST_UPT');
+		if ($result->num_rows() > 0){
+			return $result->result_array();
+		}
+		else{
+			return array();
+		}
+	}
+	
+	function getDiklat($KODE_UPT){
+	$this->db->where('KODE_UPT', $KODE_UPT);
+	$result = %this->db->get('DIKLAT_MST_DIKLAT');
+	if ($result->num_rows() > 0){
+		return $result->result_array();
+		}
+		else{
+			return array();
+		}
+	}
+
 }
 ?>
--- trunk/application/diklat/views/peserta/peserta_edit.php
+++ trunk/application/diklat/views/peserta/peserta_edit.php
@@ -1,8 +1,15 @@
 <!-- contenna -->
+<script>
+$(function() {
+$( "#TGL_LAHIR" ).datepicker();
+$( "#TGL_MASUK" ).datepicker();
+});
+</script>
+
 <div class="wrap_right bgcontent">
-	<h1 class="heading">Data Program</h1>
+	<h1 class="heading">Data Peserta</h1>
 	<hr/>
-	<?=form_open('program/proses_edit', array('class'=>'sform'))?>
+	<?=form_open('peserta/proses_edit', array('class'=>'sform'))?>
 	<fieldset>
 		<?php 
 			if(validation_errors())
@@ -17,15 +24,63 @@
 		?>
 		<ol>
 		    <input type="hidden" name="id" value="<?=$id?>">
-			<li><label for="">KODE PROGRAM <em>*</em></label> <input name="KODE_PROGRAM" value="<?=$result->row()->KODE_PROGRAM?>" type="text" class="five"/></li>
-			<li><label for="">NAMA PROGRAM <em>*</em></label> <input name="NAMA_PROGRAM" value="<?=$result->row()->NAMA_PROGRAM?>" type="text" class="five"/></li>
-			<li><label for="">SATKER <em>*</em></label>
+			
+			<li><label for="">UPT <em>*</em></label>
 				<?php 
-					$opti['name'] = 'KODE_INDUK';
-					$opti['value'] = $result->row()->KODE_INDUK;
-					echo $this->mdl_satker->getOptionUPT($opti);
+					$opti['id'] = 'KODE_UPT';
+					$opti['name'] = 'KODE_UPT';
+					$opti['value'] = $result->row()->KODE_UPT;
+					echo $this->mdl_upt->getOptionUPT($opti);
 				?>
 			</li>
+			
+			<li><label for="">DIKLAT <em>*</em></label>
+				<?php 
+					$opti['id'] = 'KODE_DIKLAT';
+					$opti['name'] = 'KODE_DIKLAT';
+					$opti['value'] = $result->row()->KODE_DIKLAT;
+					echo $this->mdl_diklat->getOptionDiklat($opti);
+				?>
+			</li>
+			
+			<li><label for="">NOMOR INDUK <em>*</em></label> <input name="NO_PESERTA" value="<?=$result->row()->NO_PESERTA?>" type="text" class="two"/></li>
+			
+			<li><label for="">NAMA PESERTA <em>*</em></label> <input name="NAMA_PESERTA" value="<?=$result->row()->NAMA_PESERTA?>" type="text" class="three"/></li>
+			
+			<li><label for="">TEMPAT LAHIR<em>*</em></label> <input name="TEMPAT_LAHIR" value="<?=$result->row()->TEMPAT_LAHIR?>" type="text" class="three"/></li>
+			
+			<li><label for="">TANGGAL LAHIR<em>*</em></label> <input name="TGL_LAHIR" value="<?=$result->row()->TGL_LAHIR?>" type="text" class="one" id="TGL_LAHIR"/></li>
+			
+			<li><label for="">DIKLAT <em>*</em></label>
+				<?php 
+					$opti['id'] = 'JK';
+					$opti['name'] = 'JK';
+					$opti['value'] = $result->row()->JK;
+					echo $this->mdl_peserta->getOptionJenkel($opti);
+				?>
+			</li>
+			
+			<li><label for="">TANGGAL MASUK<em>*</em></label> <input name="TGL_MASUK" value="<?=$result->row()->TGL_MASUK?>" type="text" class="one" id="TGL_MASUK"/></li>
+			
+			<li><label for="">TAHUN <em>*</em></label>
+				<?php 
+					$opti['id'] = 'THN_ANGKATAN';
+					$opti['name'] = 'THN_ANGKATAN';
+					$opti['value'] = $result->row()->THN_ANGKATAN;
+					echo $this->mdl_peserta->getOptionTahun($opti);
+				?>
+			</li>
+			
+			<li><label for="">STATUS PESERTA <em>*</em></label>
+				<?php 
+					$opti['id'] = 'STATUS_PESERTA';
+					$opti['name'] = 'STATUS_PESERTA';
+					$opti['value'] = $result->row()->STATUS_PESERTA;
+					echo $this->mdl_peserta->getOptionStatus($opti);
+				?>
+			</li>
+			
+			<li><label for="">KETERANGAN <em>*</em></label> <input name="KETERANGAN" value="<?=$result->row()->KETERANGAN->load()?>" type="text" class="five"/></li>
 			
 			<div class="clearfix">&nbsp;</div>
 			<hr/>
