aae4eb45839871acb4dda7bd7384e62b89573042
stefanus.christian.c <stefanus.christian.c@cafc2922-98ec-0af7-2b36-2e7d7a88d0bf>
2013-07-18 10:22:06 +0000

    Tampilkan data Peserta berdasarkan database Follow Up Iqbal

--- application/dbmon/controllers/diklat.php
+++ application/dbmon/controllers/diklat.php
@@ -44,29 +44,22 @@ class diklat extends My_Controller {
 	function peserta($upt=0, $diklat=0){
 		$this->load->model('mdl_diklat');
 		$this->load->model('mdl_upt');
-		//$this->load->model('mdl_peserta');
+		$this->load->model('mdl_peserta');
 		$this->open();
 
 		$data['title'] = 'Peserta Berdasarkan ';
 		if($upt==0){
 			$data['title'] .= 'UPT ';
-			$data['stat'] = $this->mdl_upt->getDataPeserta('');
-			//$data['statF'] = $this->mdl_upt->getData('Wanita');
-			//$data['statM'] = $this->mdl_provinsi->getData('Pria');
+			$data['tahun'] = array('2010','2011','2012','2013');
+			// Get Data berparameter (Kode Induk, Kode Program, & Tahun dalam bentuk array)
+			$data['data'] = $this->mdl_peserta->getData('5','P53',$data['tahun']);
 			$this->load->view('diklat/peserta/peserta_diklat_new', $data);
 		}
 		elseif($diklat==0){
 			$data['title'] .= 'UPT '.$this->mdl_upt->getUptByID($upt)->NAMA_UPT;
 			$data['stat'] = $this->mdl_diklat->getData($upt, '');
-			//$data['statF'] = $this->mdl_kbupaten->getData($prov, 'Wanita');
-			//$data['statM'] = $this->mdl_kbupaten->getData($prov, 'Pria');
 			$this->load->view('diklat/peserta/peserta_diklat_new', $data);
 		}
-		// else{
-		// 	$data['title'] .= 'Kabupaten/Kota '.$this->mdl_kbupaten->getKabByID($kab)->NAMAKABUP;
-		// 	$data['stat'] = $this->mdl_kbupaten->getData($prov);
-		// 	$this->load->view('sdm/dinas/sdm_kab', $data);
-		// }
 		
 		$this->close();
 	}
