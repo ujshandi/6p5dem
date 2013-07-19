94e420b444d5d331cfa77eb6c0dd8e827fa530e2
fajar.scripts@gmail.com <fajar.scripts@gmail.com@cafc2922-98ec-0af7-2b36-2e7d7a88d0bf>
2013-07-19 09:18:17 +0000

    ajax search users

---
+++ trunk/application/bboard/views/users/users_filter.php
@@ -0,0 +1,33 @@
+<script type="text/javascript">
+	$(document).ready(function() {
+		param_url='<?php echo site_url();?>/users/getall/';
+		$('#list_data').load(param_url);
+	});
+	
+	function ajaxpaging(param_url){
+		var txt_search=document.getElementById('txt_search').value;
+		if (param_url== null ){
+			param_url='<?php echo site_url();?>/users/proses_pencarian/';
+		}
+		$.ajax({
+				type: 'POST',
+				url :  param_url,
+				data: 'txt_search=' + txt_search,
+				success: function(data) {
+					$('#list_data').html(data);
+				}
+			});
+	}
+</script>
+<div class="wrap_right bgcontent">
+	<h1 class="heading">Data Users</h1>
+	<input type="text" name="txt_search" id="txt_search" value="<?=$this->session->userdata('keysearch_users'); ?>"/><!--<input type="button" onClick="ajaxpaging()" value="Cari">-->
+	<a href="#" onClick="ajaxpaging()">Cari</a>
+	<hr/>
+	
+	<a href="<?=base_url().$this->config->item('index_page').'/users/add'?>" class="control"><span class="add">Tambah Data</span></a>
+	
+	
+	<div id="list_data"></div>
+	
+</div>
