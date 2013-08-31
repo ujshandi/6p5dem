b3aa2abbae6b9de622233d2628cb48ea019383cc
fajar.scripts@gmail.com <fajar.scripts@gmail.com@cafc2922-98ec-0af7-2b36-2e7d7a88d0bf>
2013-08-31 05:03:54 +0000

    add users manajamen kompetensi

--- trunk/application/bboard/models/mdl_users.php
+++ trunk/application/bboard/models/mdl_users.php
@@ -65,10 +65,36 @@ class mdl_users extends CI_Model{
 		return $this->db->get();
 	}
 	
+	function get_item_kopeten($key,$count_data=false,$num=0, $offset=0){
+		$this->db->flush_cache();
+		$this->db->select('KOPETEN_USERS.*,KOPETEN_USER_GROUP.USER_GROUP_NAME');
+		$this->db->from('KOPETEN_USERS');
+		$this->db->join('USERS','USERS.USER_ID=KOPETEN_USERS.USER_ID');
+		$this->db->join('KOPETEN_USER_GROUP','KOPETEN_USER_GROUP.USER_GROUP_ID=KOPETEN_USERS.USER_GROUP_ID');
+		
+		if ($key!=''){
+				$this->db->like('USERNAME',$key);
+		}
+		
+		if ($count_data){
+			return $this->db->count_all_results();
+		}
+		$this->db->limit($num, $offset);
+		return $this->db->get();
+	}
+	
+	
 	function get_allitem_sdm(){
 		$this->db->flush_cache();
 		$this->db->select('*');
 		$this->db->from('SDM_USERS');
+		return $this->db->get();
+	}
+	
+	function get_allitem_kopeten(){
+		$this->db->flush_cache();
+		$this->db->select('*');
+		$this->db->from('KOPETEN_USERS');
 		return $this->db->get();
 	}
 	
@@ -203,7 +229,30 @@ class mdl_users extends CI_Model{
 			return FALSE;
 		}
 	}
-	
+	function insert_kopeten($data){
+		$this->db->flush_cache();
+		$this->db->set('USER_ID', $data['USER_ID']);
+		$this->db->set('USERNAME', $data['USERNAME']);
+		$this->db->set('USER_GROUP_ID', $data['USER_GROUP_ID']);
+		$this->db->set('NAME', $data['NAME']);
+		$this->db->set('PASSWORD', $data['PASSWORD']);
+		$this->db->set('DEPARTMENT', $data['DEPARTMENT']);
+		$this->db->set('POSITION', $data['POSITION']);
+		$this->db->set('DESCRIPTION', $data['DESCRIPTION']);
+		$this->db->set('NIP', $data['NIP']);
+		$this->db->set('EMAIL', $data['EMAIL']);
+		$this->db->set('STAT', 'A');
+		/*$this->db->set('LEVEL', $data['LEVEL']);
+		$this->db->set('KODE_UPT', $data['KODE_UPT']);*/
+		
+		$result = $this->db->insert('KOPETEN_USERS');
+		
+		if($result) {
+			return TRUE;
+		}else {
+			return FALSE;
+		}
+	}
 	
 	function get_data_edit($id){
 		$this->db->flush_cache();
@@ -282,6 +331,29 @@ class mdl_users extends CI_Model{
 		$this->db->where('USER_ID', $id);
 		$result = $this->db->delete('SDM_USERS');
 		
+		if($result) {
+			return TRUE;
+		}else {
+			return FALSE;
+		}
+	}
+	
+	function get_data_edit_kopeten($id){
+		$this->db->flush_cache();
+		$this->db->select('*');
+		$this->db->from('KOPETEN_USERS');
+		$this->db->where('USER_ID', $id);
+		
+		return $this->db->get();
+	}
+	
+
+	function update_kopeten($data){
+		$this->db->set('USER_GROUP_ID', $data['USER_GROUP_ID']);
+		/*$this->db->set('LEVEL', $data['LEVEL']);*/
+		
+		$this->db->where('USERNAME', $data['USERNAME']);
+		$result = $this->db->update('KOPETEN_USERS');
 		if($result) {
 			return TRUE;
 		}else {
@@ -454,6 +526,56 @@ class mdl_users extends CI_Model{
 				$out .= '<option value="'.$r->USER_GROUP_ID.'" selected="selected">'.$r->USER_GROUP_NAME.'</option>';
 			}else{
 				$out .= '<option value="'.$r->USER_GROUP_ID.'">'.$r->USER_GROUP_NAME.'</option>';
+			}
+		}
+		$out .= '</select>';
+		
+		return $out;
+	}
+	
+	function get_group_user_kopeten($d=""){
+		$name = isset($d['name'])?$d['name']:'';
+		$id = isset($d['id'])?$d['id']:'';
+		$class = isset($d['class'])?$d['class']:'';
+		$value = isset($d['value'])?$d['value']:'';
+		
+		$this->db->flush_cache();
+		$this->db->from('KOPETEN_USER_GROUP');
+		
+		$res = $this->db->get();
+		
+		$out = '<select name="'.$name.'" id="'.$id.'">';
+		foreach($res->result() as $r){
+			if($r->USER_GROUP_ID == trim($value)){
+				$out .= '<option value="'.$r->USER_GROUP_ID.'" selected="selected">'.$r->USER_GROUP_NAME.'</option>';
+			}else{
+				$out .= '<option value="'.$r->USER_GROUP_ID.'">'.$r->USER_GROUP_NAME.'</option>';
+			}
+		}
+		$out .= '</select>';
+		
+		return $out;
+	}
+	
+	function get_user_not_in_kopeten($d="",$arr_users=array()){
+		$name = isset($d['name'])?$d['name']:'';
+		$id = isset($d['id'])?$d['id']:'';
+		$class = isset($d['class'])?$d['class']:'';
+		$value = isset($d['value'])?$d['value']:'';
+		
+		$this->db->flush_cache();
+		$this->db->from('USERS');
+		if (count($arr_users)>0){
+			$this->db->where_not_in('USER_ID' ,  $arr_users);
+		}
+		$res = $this->db->get();
+		
+		$out = '<select name="'.$name.'" id="'.$id.'">';
+		foreach($res->result() as $r){
+			if($r->USER_ID == trim($value)){
+				$out .= '<option value="'.$r->USER_ID.'" selected="selected">'.$r->USERNAME.'</option>';
+			}else{
+				$out .= '<option value="'.$r->USER_ID.'">'.$r->USERNAME.'</option>';
 			}
 		}
 		$out .= '</select>';
