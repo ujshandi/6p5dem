1b675410360d5b899b214c70ef2f1b87949daa8e
stefanus.christian.c <stefanus.christian.c@cafc2922-98ec-0af7-2b36-2e7d7a88d0bf>
2013-10-16 11:03:03 +0000

    GIS V1

---
+++ application/bboard/controllers/peserta_frontpage.php
@@ -0,0 +1,184 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Peserta_frontpage extends My_Frontpage {
+
+	var $id = 'peserta';
+	
+	function __construct(){
+		parent::__construct();
+		$this->load->model('mdl_satker');
+		$this->load->model('mdl_upt');
+		$this->load->model('mdl_diklat');
+		$this->load->model('mdl_peserta');
+		$this->load->library('pagination');
+		$this->load->model('mdl_peserta_front');
+		$this->load->helper('url');
+		
+	}
+	
+	function test(){
+		echo 'hahaha';
+	}
+	
+	public function index()
+	{
+		$this->open();
+		
+		# get filter
+		$data['kode_upt'] = $this->session->userdata($this->id.'kode_upt');
+		$data['search'] = $this->session->userdata($this->id.'search');
+		$data['numrow'] = $this->session->userdata($this->id.'numrow');
+		$data['numrow'] = !empty($data['numrow'])?$data['numrow']:30;
+		$offset = ($this->uri->segment(3))?$this->uri->segment(3):0;
+		
+		# get data
+		$result = $this->mdl_peserta_front->getData($data['numrow'], $offset, $data);
+		
+		# config pagination
+		$config['base_url'] = base_url().'/'.$this->config->item('index_page').'/peserta_frontpage/index/';
+		$config['per_page'] = $data['numrow'];
+		$config['num_links'] = '10';
+		$config['uri_segment'] = '3';
+		$config['full_tag_open'] = '';
+		$config['full_tag_close'] = '';
+		$config['num_tag_open'] = '<li>';
+		$config['num_tag_close'] = '</li>';
+		$config['cur_tag_open'] = '<li class="active"><a href="javascript:void(0)" class="current">';
+		$config['cur_tag_close'] = '</a></li>';
+		$config['prev_link'] = 'Prev';
+		$config['prev_tag_open'] = '<li>';
+		$config['prev_tag_close'] = '</li>';
+		$config['next_link'] = 'Next';
+		$config['next_tag_open'] = '<li>';
+		$config['next_tag_close'] = '</li>';
+		$config['last_link'] = 'Last';
+		$config['last_tag_open'] = '<li>';
+		$config['last_tag_close'] = '</li>';
+		$config['first_link'] = 'First';
+		$config['first_tag_open'] = '<li>';
+		$config['first_tag_close'] = '</li>';
+
+		$config['total_rows'] = $result['row_count'];
+
+		$this->pagination->initialize($config);	
+		
+		$data['curcount'] = $offset+1;
+		$data['result'] = $result['row_data'];
+		
+		$this->load->view('info_peserta/peserta_list', $data);
+		
+		$this->close();
+	}
+	
+	public function search(){
+		$this->session->set_userdata($this->id.'kode_upt', $this->input->post('kode_upt'));
+		$this->session->set_userdata($this->id.'search', $this->input->post('search'));
+		$this->session->set_userdata($this->id.'numrow', $this->input->post('numrow'));
+		
+		redirect('peserta_frontpage');
+	}
+	
+	public function hid_filter($kodeupt){
+		$this->session->set_userdata($this->id.'kode_upt', $kodeupt);
+		$this->session->set_userdata($this->id.'search', '');
+		$this->session->set_userdata($this->id.'numrow', 30);
+		
+		//echo $kodeupt;
+		redirect('peserta_frontpage');
+	}
+	
+	/*public function add_alumni1(){
+		$this->open();		
+		$this->load->view('alumni/alumni_add1');
+		$this->close();
+	}
+	
+	public function add_alumni2(){
+		$this->open();
+		
+		$data['KODE_UPT'] 		= $this->input->post('KODE_UPT');
+		$data['KODE_DIKLAT'] 	= $this->input->post('KODE_DIKLAT');
+		$data['THN_ANGKATAN'] 	= $this->input->post('THN_ANGKATAN');
+		$data['TGL_LULUS'] 		= $this->input->post('TGL_LULUS');
+		$data['KERJA'] 			= $this->input->post('KERJA');
+		$data['INSTANSI'] 		= $this->input->post('INSTANSI');
+	
+		if($data['KODE_DIKLAT'] == '' || $data['THN_ANGKATAN'] == '0'){
+			redirect('alumni/add_alumni1');
+		}
+		
+		
+		$data['UPT'] = $this->mdl_upt->getDataEdit($data['KODE_UPT']);
+		$data['DIKLAT'] = $this->mdl_diklat->getDataEdit($data['KODE_DIKLAT']);
+		$data['data'] = $this->mdl_peserta->getPesertaRegister($data['KODE_UPT'], $data['KODE_DIKLAT'], $data['THN_ANGKATAN'], $data['TGL_LULUS'], $data['KERJA'], $data['INSTANSI']);
+		
+		$this->load->view('alumni/alumni_add2', $data);
+		$this->close();
+	}
+	
+	public function proses_add_alumni1(){
+		$data['DATA'] = $this->input->post('DATA');
+		
+		if($this->mdl_alumni->UpdateAlumni($data['DATA'])){
+			redirect('alumni');
+		}else{
+			echo 'Error insert to db!';
+		}
+		
+	}
+	
+	public function edit($id){
+		$this->open();
+		
+		$data['id'] = $id;
+		$data['result'] = $this->mdl_alumni->getDataEdit($id);
+		$this->load->view('alumni/alumni_edit', $data);
+		
+		$this->close();
+	}
+	
+	public function proses_edit(){
+		$this->open();
+		
+		$data['id'] = $this->input->post('id');
+		$data['KODE_UPT'] = $this->input->post('KODE_UPT');
+        $data['IDPESERTA'] = $this->input->post('IDPESERTA');
+        $data['TGL_LULUS'] = "to_date('".$this->input->post('TGL_LULUS')."', 'mm/dd/yyyy')";
+        $data['KERJA'] = $this->input->post('KERJA');
+        $data['INSTANSI'] = $this->input->post('INSTANSI');
+		
+		# set rules validation
+		$this->form_validation->set_rules('KODE_UPT', 'UPT', 'required');
+        $this->form_validation->set_rules('IDPESERTA', 'PESERTA', 'required');
+        $this->form_validation->set_rules('TGL_LULUS', 'KODE INDUK', 'required');
+        $this->form_validation->set_rules('KERJA', 'TEMPAT KERJA', 'required');
+        $this->form_validation->set_rules('INSTANSI', 'INSTANSI', 'required');
+		# set message validation
+		$this->form_validation->set_message('required', 'Field %s harus diisi!');
+		
+		if ($this->form_validation->run() == FALSE){
+			$this->load->view('alumni/alumni_edit',$data);
+		}else{
+			$this->mdl_alumni->update($data);
+			redirect('alumni');
+		}
+		
+		$this->close();
+	}
+	
+	public function proses_delete($id){
+		if($this->mdl_alumni->delete($id)){
+			redirect('alumni');
+		}else{
+			
+		}
+	}*/
+	
+	function getPeserta(){
+		$opt['name'] = 'KODE_UPT';
+		$opt['id'] = 'KODE_UPT';
+		$opt['KODE_UPT'] = $this->input->post('KODE_UPT');
+		echo $this->mdl_alumni->getOptionPesertaByUPT($opt);
+	}
+	
+}
---
+++ application/bboard/views/info_peserta/peserta_list.php
@@ -0,0 +1,74 @@
+<!-- contenna -->
+<div class="wrap_right bgcontent">
+	<h1 class="heading">Informasi Data Alumni</h1>
+	<hr/>
+	<a href="<?=site_url();?>" class="greenbutton">Back</a>
+	<br>
+	<br>
+	<?=form_open('peserta_frontpage/search', array('class'=>'sform'))?>
+	<fieldset>
+	<ol>
+		<li>
+			UPT : 
+			<select name="kode_upt">
+				<?=$this->mdl_upt->getOptionUPT(array('value'=>$kode_upt))?>
+			</select>
+			&nbsp;&nbsp;
+			NAMA PESERTA :
+			<input type="textfield" name="search" value="<?=!empty($search)?$search:''?>" />
+			&nbsp;&nbsp;
+			<select name="numrow">
+				<option value="30" <?=$numrow==30?'Selected="selected"':''?>>30</option>
+				<option value="50" <?=$numrow==50?'Selected="selected"':''?>>50</option>
+				<option value="75" <?=$numrow==75?'Selected="selected"':''?>>75</option>
+				<option value="100" <?=$numrow==100?'Selected="selected"':''?>>100</option>
+				<option value="200" <?=$numrow==200?'Selected="selected"':''?>>200</option>
+			</select>
+			<input type="submit" name="submit" value="Proses" />
+		</li>
+	</ol>		
+	</fieldset>
+	<?=form_close()?>
+	
+	<hr/>
+	<table width="100%">
+	  <thead>
+		<th>NO</th>
+		<th width="20%">UPT</th>
+		<th width="15%">DIKLAT</th>
+		<th>NO PESERTA</th>
+		<th width="15%">NAMA PESERTA</th>
+		<th>TAHUN ANGKATAN</th>
+	  </thead>
+	  <tbody>
+	  
+		<?
+		$i=$curcount;
+		foreach($result->result() as $r){
+		?>
+		
+			<tr class='gradeC'>
+				<td width='2%'><?=$i?></td>
+				<td width='15%'><?=$r->NAMA_UPT?></td>
+				<td width='15%'><?=$r->NAMA_DIKLAT?></td>
+				<td width='10%'><?=$r->NO_PESERTA?></td>
+				<td width='20%'><?=$r->NAMA_PESERTA?></td>
+				<td width='15%'><?=$r->THN_ANGKATAN?></td>
+			</tr>
+		<?
+		$i++;
+		}
+		?>
+	  </tbody>
+	</table>
+	
+          	<div class="clearfix">&nbsp;</div>        
+        <div class="paging right">
+          <ul>
+            <li class="active">
+				 <li><?=$this->pagination->create_links()?></li>
+          </ul>
+        </div><!--end pagination-->
+	<div class="clearfix"></div>
+        
+</div><!-- end wrap right content-->
