e9e6ab45f51cfcd4e38b150d7bd3798a3095acaf
stefanus.christian.c <stefanus.christian.c@cafc2922-98ec-0af7-2b36-2e7d7a88d0bf>
2013-10-20 14:14:19 +0000

    GIS Update

--- gis.php
+++ gis.php
@@ -2,5 +2,5 @@
 	$base_url  = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
 	$base_url .= "://".$_SERVER['HTTP_HOST'];
 	$base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']); 
-     header( 'Location: '.$base_url.'application/gis/index-gis2.php' );
+     header( 'Location: '.$base_url.'application/gis/index-gis3.php' );
 ?>
